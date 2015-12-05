03609d4b6415a266a8bd76b9ed00e40026d911f3
Kanke <kanke.ishaku@netbuilder.com>
2015-12-05 21:25:19 +0000

    Myhumm sdk implemented by myhumm guy

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'myhumm' do
+
+ pod "AFNetworking", "~> 2.0"
+ pod 'JSONModel'
+
+end
+
+target 'myhummTests' do
+
+end
+
+target 'myhummUITests' do
+
+end
+
--- myhumm/ViewController.m
+++ myhumm/ViewController.m
@@ -7,21 +7,65 @@
 //
 
 #import "ViewController.h"
-
+#import "HummAPI.h"
+#import "PlaylistsAPI.h"
+#import "Song.h"
 @interface ViewController ()
+
+@property (nonatomic) HummAPI *humm;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.humm = [HummAPI sharedManager];
     // Do any additional setup after loading the view, typically from a nib.
+    [self authenticateHumm];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)authenticateHumm{
+    
+    [self.humm loginWithUsername:@"kanke" password:@"Windows6" onLoginSuccess:^{
+        [self getPlaylist];
+    } onLoginError:^(NSError *error) {
+        
+    }];
+    
+    
+}
+
+-(void)getPlaylist{
+   
+    [self.humm.playlists getSongs:@"56403fd834017507dba11880" limit:20 offset:0 success:^(NSArray<Song *> *response) {
+
+        for (Song  *song in response)
+        {
+            NSLog(@"song name = %@", song.title);
+        }
+        
+    } error:^(NSError *error) {
+        
+    }];
+}
+
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//
+//
+//}
+//
+//-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+//
+//    
+//}
 
 @end
